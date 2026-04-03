4c51b98de69b74e9608df9d8b188a52f48925516
aproposito <aproposito@gmail.com>
2026-04-03 11:18:33 +0200

    Update queries.js_5

--- query/queries.js
+++ query/queries.js
@@ -29,28 +29,28 @@ db.restaurants.find({"grades": { $elemMatch: { "score": {$gt : 80, $lt : 100}} }
 db.restaurants.find({"location.coordinates.0": { $lt : -95.754168}}, {"_id":0})
 
 // 11. Trobar restaurants que no preparen 'American', amb qualificació > 70 i longitud < -65.754168.
-
+db.restaurants.find({$and:  [{"cuisine": {$ne:  "American"}}, {"grades.score": {$gt: 70}}, {"location.coordinates.0": {$lt:-65.754168 }}]}, {"_id":0})
 
 // 12. El mateix que l'anterior però sense usar operador $and.
-
+db.restaurants.find({"cuisine": {$ne:  "American"}, "grades.score": {$gt: 70}, "location.coordinates.0": {$lt:-65.754168 }}, {"_id":0})
 
 // 13. Trobar restaurants que no són 'American', grau 'A', i no són de Brooklyn. Ordenats per cuisine descendent.
-
+db.restaurants.find({"cuisine": {$ne: "American"} , "grades.grade" : "A", "borough": {$ne: "Brooklyn"}}, {"_id":0}).sort({"cuisine":-1})
 
 // 14. Trobar restaurant_id, name, borough i cuisine on el nom comença amb 'Wil'.
-
+db.restaurants.find({"name" : /^Wil/}, {"_id":0, "restaurant_id":1, "name": 1, "borough":1, "cuisine":1})
 
 // 15. Trobar restaurant_id, name, borough i cuisine on el nom acaba en 'ces'.
-
+db.restaurants.find({"name" : /ces$/}, {"_id":0, "restaurant_id":1, "name": 1, "borough":1, "cuisine":1})
 
 // 16. Trobar restaurant_id, name, borough i cuisine on el nom conté 'Reg'.
-
+db.restaurants.find({"name" : /Reg/}, {"_id":0, "restaurant_id":1, "name": 1, "borough":1, "cuisine":1})
 
 // 17. Trobar restaurants del Bronx que preparen cuina americana o xinesa.
-
+db.restaurants.find({"borough":"Bronx", "cuisine": {$in:["American", "Chinese"]}}, {"_id":0})
 
 // 18. Trobar restaurant_id, name, borough i cuisine per a Staten Island, Queens, Bronx o Brooklyn.
-
+db.restaurants.find({"borough": {$in: ["Staten Island", "Queens", "Bronx", "Brooklyn"]}}, {"_id":0, "restaurant_id":1, "name": 1, "borough":1, "cuisine":1})
 
 // 19. Trobar restaurant_id, name, borough i cuisine per a restaurants que NO són d'aquests barris.
 
